e57d6972d88397c15ac3f20bc639d4e95cab1535
Parker Sprouse <parker.sprouse@buspatrol.com>
2022-12-30 00:17:07 -0600

    Auto-refresh implemented

--- apps/web/assets/src/javascripts/controllers/refresh_all_controller.js
+++ apps/web/assets/src/javascripts/controllers/refresh_all_controller.js
@@ -1,11 +1,25 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class RefreshAllController extends Controller {
+  connect() {
+    const refresh_interval = Number(window.RssReader?.preferences?.refresh_interval);
+    if (refresh_interval && refresh_interval > 0) {
+      // Refresh interval is stored in seconds, but the JS interval method uses milliseconds
+      this.refresher = setInterval(() => {
+        this.action();
+      }, refresh_interval * 1000);
+    }
+  }
+
+  disconnect() {
+    if (this.refresher) clearInterval(this.refresher);
+  }
+
   action() {
-    window.Sortable.toArray().forEach((feed) => {
-      let selector = `#${feed} .gra-card-title.hidden-mobile span[data-controller="refresh"]`;
+    window.Sortable.toArray().forEach((id) => {
+      let selector = `#${id} .gra-card-title.hidden-mobile span[data-controller="refresh"]`;
       if (window.matchMedia('(max-width: 767px)').matches) {
-        selector = `#${feed} .gra-card-title.visible-mobile span[data-controller="refresh"]`;
+        selector = `#${id} .gra-card-title.visible-mobile span[data-controller="refresh"]`;
       }
 
       const ele = document.querySelector(selector);
