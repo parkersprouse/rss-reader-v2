9b45e0a45a1ff65a9dc22bbad2d7ac11ee1d0530
Parker Sprouse <parker.sprouse@buspatrol.com>
2022-12-31 10:55:10 -0600

    Unified settings modal

--- apps/web/assets/src/javascripts/controllers/modal_controller.js
+++ apps/web/assets/src/javascripts/controllers/modal_controller.js
@@ -3,9 +3,11 @@ import { Controller } from '@hotwired/stimulus';
 export default class ModalController extends Controller {
   open() {
     document.querySelector(`#${this.element.dataset.id}`).classList.add('open');
+    document.querySelector('body').style.overflow = 'hidden';
   }
 
   close() {
     document.querySelector(`#${this.element.dataset.id}`).classList.remove('open');
+    document.querySelector('body').style.removeProperty('overflow');
   }
 };
