538a136ed7b48ec202067b789b68c66f8e5cc45a
Parker Sprouse <parker@parkersprouse.me>
2024-11-15 16:35:01 -0600

    A whole bunch of fancy stuff

--- apps/web/assets/src/javascripts/controllers/load_feed_controller.js
+++ apps/web/assets/src/javascripts/controllers/load_feed_controller.js
@@ -1,11 +1,18 @@
 ﻿import { Controller } from '@hotwired/stimulus';
 
 export default class LoadFeedController extends Controller {
-  connect() {
-    this.load();
+  async connect() {
+    await this.load();
+
+    this.titles = this.element.getElementsByClassName('feed-title');
+    for (const ele of this.titles) ele.addEventListener('click', this.titleClickHandler.bind(this));
   }
 
-  load() {
+  disconnect() {
+    this.unload();
+  }
+
+  async load() {
     const { id } = this.element;
 
     const stored_feed = JSON.parse(window.sessionStorage.getItem(id));
@@ -14,24 +21,33 @@ export default class LoadFeedController extends Controller {
       return;
     }
 
-    fetch(`/feeds/${id.replace('feed-', '')}`)
-      .then(response => response.text())
-      .then((html) => {
-        window.sessionStorage.setItem(id, JSON.stringify({ body: html, exp: Date.now() + (1000 * 60 * 5) }));
-        this.element.innerHTML = html;
-      })
-      .catch(() => {
-        this.element.innerHTML = `
-          <div class='gra-card'>
-            <div class='gra-card-content'>
-              <h5 class='gra-card-title'>
-                Failed To Load Feed
-              </h5>
-              <div class='gra-card-body' id='show-${id}'>
-                There was a problem loading the feed
-              </div>
+    try {
+      const response = await fetch(`/feeds/${id.replace('feed-', '')}`);
+      const html = await response.text();
+      window.sessionStorage.setItem(id, JSON.stringify({ body: html, exp: Date.now() + (1000 * 60 * 5) }));
+      this.element.innerHTML = html;
+    } catch {
+      this.element.innerHTML = `
+        <div class='gra-card'>
+          <div class='gra-card-content'>
+            <h5 class='gra-card-title'>
+              Failed To Load Feed
+            </h5>
+            <div class='gra-card-body' id='show-${id}'>
+              There was a problem loading the feed
             </div>
-          </div>`;
-      });
+          </div>
+        </div>`;
+    }
+  }
+
+  unload() {
+    // this.title.removeEventListener('click', this.titleClickHandler);
+    for (const ele of this.titles) ele.removeEventListener('click', this.titleClickHandler);
+  }
+
+  titleClickHandler() {
+    console.log('clicked');
+    for (const ele of this.titles) ele.classList.toggle('extended');
   }
 };
